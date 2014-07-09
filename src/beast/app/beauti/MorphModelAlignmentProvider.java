a6b7cc3131c243396bc76f7c92f15f89dff22625
rbouckaert <higgs.ml@gmail.com>
2014-07-10 11:14:52 +1200

    initial set-up

---
+++ src/beast/app/beauti/MorphModelAlignmentProvider.java
@@ -0,0 +1,9 @@
+package beast.app.beauti;
+
+import beast.core.Description;
+
+@Description("Class for creating new partitions for morphological data to be edited by AlignmentListInputEditor")
+public class MorphModelAlignmentProvider extends BeautiAlignmentProvider {
+	
+
+}
